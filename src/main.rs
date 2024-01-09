df99d32fae01c160aa2085ff2323a3cb0e7bd375
benAmi <minebenking@gmail.com>
2024-01-09 21:33:09 +0200

    my bad lol

--- src/main.rs
+++ src/main.rs
@@ -26,13 +26,6 @@ int main(int argc, char **argv) {
     std::cout << "Hello, World\n";
     return 0;
 }
-let filename = PathBuf::from(format!("{}/{}.{}", root.to_str().unwrap(), MAIN, &args.lang));
-let mut file = OpenOptions::new()
-	.write(true)
-	.create(true)
-	.open(filename)?;
-
-match args.lang.as_str
 "#;
 
 #[derive(Parser)]
